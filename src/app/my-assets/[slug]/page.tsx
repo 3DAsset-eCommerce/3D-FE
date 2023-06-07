f3f369c42e5406004e4657d891a55b98fdad0894
Jo Young <githuborigin@gmail.com>
2023-06-07 16:16:18 +0900

    feat: :sparkles: test

--- src/app/my-assets/[slug]/page.tsx
+++ src/app/my-assets/[slug]/page.tsx
@@ -8,5 +8,5 @@ import { MyAssetFooter } from '@/components/myasset/MyAssetFooter'
 import { MyAssetPagenation } from '@/components/myasset/MyAssetPagenation'
 export default function MyAssetsPage() {
   alert('page')
-  return <Layout>asddd</Layout>
+  return <Layout>테스트 테스트</Layout>
 }
