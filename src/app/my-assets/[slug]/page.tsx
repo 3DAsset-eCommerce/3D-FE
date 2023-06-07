c042b7c7c83bd7b22b2f080992d105ae7f8ee232
Jo Young <githuborigin@gmail.com>
2023-06-07 16:03:29 +0900

    feat: :sparkles: asd

--- src/app/my-assets/[slug]/page.tsx
+++ src/app/my-assets/[slug]/page.tsx
@@ -7,24 +7,6 @@ import { MyAssetContetntsItem } from '@/components/myasset/MyAssetContetntsItem'
 import { MyAssetFooter } from '@/components/myasset/MyAssetFooter'
 import { MyAssetPagenation } from '@/components/myasset/MyAssetPagenation'
 export default function MyAssetsPage() {
-  alert(page)
-  return (
-    <Layout>
-      <div className="flex h-full ">
-        <div className="w-full flex-auto p-10">
-          <MyAssetContents>
-            <MyAssetContetntsItem />
-          </MyAssetContents>
-          <MyAssetPagenation />
-          <MyassetButtons />
-        </div>
-        <div className="flex-1">
-          <Drawer>
-            <AssetDetail />
-          </Drawer>
-        </div>
-      </div>
-      <MyAssetFooter />
-    </Layout>
-  )
+  alert('page')
+  return <Layout>asd</Layout>
 }
