9b9d98dc8a1677bc22242e9fc577a5c9dbcbe4b4
ronieo <romelzen50@gmail.com>
2023-07-07 18:32:46 +0900

    feat: :sparkles: 내에셋 쿼리함수 수정 중

--- src/hooks/useGetMyAssets.ts
+++ src/hooks/useGetMyAssets.ts
@@ -3,15 +3,56 @@ import { RootState } from '@/store/store'
 import { useQuery } from '@tanstack/react-query'
 import { useSelector } from 'react-redux'
 
-export const useGetMyAssets = ({ page, size }: { page: number; size: number }) => {
+export const useGetMyAssets = (page: number, size: number) => {
   const userId = useSelector((state: RootState) => state.user.userId)
 
   const { data: myAssets, isLoading } = useQuery({
     queryKey: ['assets', page],
     queryFn: () => getMyAssets(userId, page, size),
+    // queryFn: () => getMyAssets(userId || 0, page, size),
     keepPreviousData: true,
   })
-  console.log(myAssets, isLoading, '<<<<<<<< myAssets Data')
 
   return { myAssets, isLoading }
 }
+
+// import { getMyAssets } from '@/api/service/myAsset'
+// import { RootState } from '@/store/store'
+// import { useQuery } from '@tanstack/react-query'
+// import { useSelector } from 'react-redux'
+
+// export const useGetMyAssets = (page: number, size: number) => {
+//   const userId = useSelector((state: RootState) => state.user.userId)
+
+//   const { data: myAssets, isLoading } = useQuery({
+//     queryKey: ['assets', page],
+//     queryFn: () => getMyAssets(userId, page, size),
+//     keepPreviousData: true,
+//   })
+
+//   return { myAssets, isLoading }
+// }
+// import { getMyAssets } from '@/api/service/myAsset'
+// import { RootState } from '@/store/store'
+// import { useQuery } from '@tanstack/react-query'
+// import { useSelector } from 'react-redux'
+
+// export const useGetMyAssets = (id: number | null, page: number, size: number) => {
+//   const { data: myAssets, isLoading } = useQuery({
+//     queryKey: ['myAssets', id, page],
+//     queryFn: () => getMyAssets(id, page, size),
+//     keepPreviousData: true,
+//   })
+
+//   return { myAssets, isLoading }
+// }
+
+// export const useGetMyAssets = (page: number, size: number) => {
+//   const userId = useSelector((state: RootState) => state.user.userId)
+//   const { data: myAssets, isLoading } = useQuery({
+//     queryKey: ['myAssets', page],
+//     queryFn: () => getMyAssets(userId, page, size),
+//     keepPreviousData: true,
+//   })
+//   return { myAssets, isLoading }
+// }
