32e88fc98224f55bf0e8a001d819ed0dbb571b8a
ronieo <romelzen50@gmail.com>
2023-07-07 18:34:10 +0900

    feat: :sparkles: myAssetsPage  props추가

--- src/app/my-assets/page.tsx
+++ src/app/my-assets/page.tsx
@@ -3,18 +3,23 @@ import AssetDetail from '@/components/assetDetail/AssetDetail'
 import Drawer from '@/components/common/Drawer'
 import MyAsset from '@/components/myAsset/MyAsset'
 
-export default function MyAssetsPage() {
+interface MyAssetsPageProps {
+  id: number | null
+}
+
+export default function MyAssetsPage({ id }: MyAssetsPageProps) {
   return (
     <Layout>
       <div className="flex">
         <section className="min-h-[calc(100vh-12.3rem)] px-[2.3rem] pt-8">
+          {/* <MyAsset id={id} /> */}
           <MyAsset />
         </section>
-        <section className="sticky top-[7.2rem] h-[calc(100vh-7.2rem)] ">
+        {/* <section className="sticky top-[7.2rem] h-[calc(100vh-7.2rem)] ">
           <Drawer>
             <AssetDetail />
           </Drawer>
-        </section>
+        </section> */}
       </div>
     </Layout>
   )
